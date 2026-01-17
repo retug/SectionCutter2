ada7cfd0a16668c88d49360e62780d886945718e
Austin Guter <austinguter@gmail.com>
2026-01-17 10:27:55 -0500

    update for close and reopen.

--- SectionCutter/Class1.cs
+++ SectionCutter/Class1.cs
@@ -1,37 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 using ETABSv1;
 
 namespace SectionCutter
 {
-
     class cPlugin
     {
+        private static readonly object _sync = new object();
+
+        private static Thread _uiThread;
+        private static Dispatcher _uiDispatcher;
+
+        private static MainWindow _mainWindow;
+
         public void Main(ref cSapModel SapModel, ref cPluginCallback ISapPlugin)
         {
-            // Copy to locals (safe inside thread) test test
             var localSapModel = SapModel;
             var localPlugin = ISapPlugin;
 
-            Thread uiThread = new Thread(() =>
+            EnsureUiThreadStarted();
+
+            // Always marshal to the UI dispatcher thread
+            _uiDispatcher.BeginInvoke(new Action(() =>
             {
-                var app = new Application();
-                var window = new MainWindow(localSapModel, localPlugin);
-                app.Run(window);
-            });
+                ShowOrActivateWindow(localSapModel, localPlugin);
+            }));
+        }
 
-            uiThread.SetApartmentState(ApartmentState.STA);
-            uiThread.Start();
+        private static void EnsureUiThreadStarted()
+        {
+            lock (_sync)
+            {
+                if (_uiThread != null && _uiThread.IsAlive && _uiDispatcher != null)
+                    return;
 
+                _uiThread = new Thread(() =>
+                {
+                    // Create dispatcher for this STA thread
+                    _uiDispatcher = Dispatcher.CurrentDispatcher;
+
+                    // Start message loop for this thread
+                    Dispatcher.Run();
+                });
+
+                _uiThread.SetApartmentState(ApartmentState.STA);
+                _uiThread.IsBackground = true;
+                _uiThread.Start();
+
+                // Wait until dispatcher is ready
+                while (_uiDispatcher == null)
+                    Thread.Sleep(10);
+            }
+        }
+
+        private static void ShowOrActivateWindow(cSapModel sapModel, cPluginCallback plugin)
+        {
+            // If the old window exists, but is closed/not loaded, drop it
+            if (_mainWindow != null && !_mainWindow.IsLoaded)
+                _mainWindow = null;
+
+            if (_mainWindow == null)
+            {
+                _mainWindow = new MainWindow(sapModel, plugin);
+
+                // When closed, allow reopening later
+                _mainWindow.Closed += (_, __) =>
+                {
+                    _mainWindow = null;
+                };
+
+                _mainWindow.Show();
+                _mainWindow.Activate();
+                return;
+            }
+
+            // Bring to front if already open
+            if (_mainWindow.WindowState == WindowState.Minimized)
+                _mainWindow.WindowState = WindowState.Normal;
+
+            _mainWindow.Activate();
+            _mainWindow.Topmost = true;
+            _mainWindow.Topmost = false;
+            _mainWindow.Focus();
         }
 
         public long Info(ref string Text)
         {
-            Text = "Section Cutter Tool. Diaphragm Slicer";
+            Text = "Section Cutter Tool. Diaphragm Slicer.";
             return 0;
         }
     }
